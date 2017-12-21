302ae89c0fe02bb46a598fbe4c1434d92dc58395
Alina Zhulanova <alina.zhulanava@gmail.com>
2017-12-21 07:46:18 +0300

    first

---
+++ lab8/lab8/ShiftF2.cs
@@ -0,0 +1,40 @@
+﻿using EventHook;
+
+namespace lab8
+{
+    class ShiftF2
+    {
+        private bool _shiftPressed;
+        private bool _somethingElse;
+
+        private const string LShiftKeyName = "LeftShift";
+        private const string RShiftKeyName = "RightShift";
+        private const string F2 = "VolumeDown";
+
+        public void AnalyseEvent(KeyData keyData, Form1 form1)
+        {
+            var isKeyDownEvent = keyData.EventType.ToString().Equals("down");
+
+            switch (keyData.Keyname)
+            {
+                case RShiftKeyName:
+                case LShiftKeyName:
+                    _somethingElse = false;
+                    _shiftPressed = isKeyDownEvent;
+                    break;
+
+                case F2:
+                    if (_shiftPressed && isKeyDownEvent && !_somethingElse)
+                    {
+                        _shiftPressed = false;
+                        form1.IsVisible = !form1.IsVisible;
+                    }
+                    break;
+
+                default:
+                    _somethingElse = true;
+                    break;
+            }
+        }
+    }
+}
